ffe50a0584c2646ea022130df02a72f85ffb8567
MeilisaApriyani <melisa.2005.apriyani@gmail.com>
2025-10-27 09:57:34 +0700

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const database = async () => {
+    try {
+        console.log("Starting Connection to MongoDB")
+
+        const response = await mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8")
+
+        console.log("Koneksi ke database Berhasil")
+
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
